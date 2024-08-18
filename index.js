59941cfaef15595f4763ac88b1bb4cc992d87f65
Gur Shalev <shalevgur1@gmail.com>
2024-08-18 23:26:38 +0300

    add axios and add initial resault page file

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+// A website that provides various kinds of plants based on user-submitted requirements,
+// such as hardiness (climate zone), watering needs, quantity of edible plants, etc.
+// This website uses the Perenual.com API to retrieve data from a plant database
+// and makes this information accessible to the end user in a user-friendly way.
+
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 // Seting global constants
 const port = 3000;
@@ -17,9 +23,17 @@ app.listen(port, () => {
 
 // Handeling different HTTP requests
 app.get("/", (req, res) => {
+    // Rendering home page
     res.render("index.ejs");
 });
 
 app.get("/design-request", (req, res) => {
+    // Rendering design-request page with design form
     res.render("designRequest.ejs");
+});
+
+app.post("/send-design-request", (req, res) => {
+    // Sending a request to perenual.com API and rendering 
+    // the resault page with the retrived data.
+    res.render("resultPlantList.ejs");
 });
